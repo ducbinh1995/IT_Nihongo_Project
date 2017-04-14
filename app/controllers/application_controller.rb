78e47c70a364b086502222dcfb8fb2efa6a14f35
ducbinh1995 <binhbeo1711@gmail.com>
2017-04-14 23:11:17 +0700

    Edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: [:name, :gender]
-    devise_parameter_sanitizer.permit :account_update, keys: [:name, :gender]
+    devise_parameter_sanitizer.permit :account_update, keys: [:name, :gender, :avatar, :avatar_cache, :remove_avatar]
   end
 end
