0f7ab9d6a7d7a67fc6ab877f24aa970a52122fb8
ducbinh1995 <binhbeo1711@gmail.com>
2017-04-22 11:12:07 +0700

    Delete image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -63,7 +63,7 @@ class ImagesController < ApplicationController
   def destroy
     @image.destroy
     respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+      format.html { redirect_to "", notice: 'Image was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
