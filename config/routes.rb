a6369f681212ef7daf401e814e328819e62aa345
ducbinh1995 <binhbeo1711@gmail.com>
2017-04-28 23:03:28 +0700

    Redirect edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :animals
   root 'static_pages#home'
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "my_devise/registrations"}
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
